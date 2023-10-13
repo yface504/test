71575cc5f427464c2838b2c4fa59af5fc1f93b65
even504@hotmail.com.tw <even504@hotmail.com.tw>
2023-10-13 17:29:21 +0800

    匯率1005

--- 1.py
+++ 1.py
@@ -3,6 +3,9 @@ import calendar
 import requests
 from bs4 import BeautifulSoup
 
+# 获取用户输入
+user_input = input("请输入 a~g: ")
+
 # 将字符串"20230901"转换为日期格式
 date_str = "20230801"
 new_date = datetime.strptime(date_str, "%Y%m%d")
@@ -15,38 +18,83 @@ wocha = "https://chl.cn/?" + str(new_date.year) + "-" + str(new_date.month) + "-
 
 # 发送HTTP请求并获取页面内容
 response = requests.get(wocha)
-response.encoding = 'utf-8' #改變編碼解決亂碼問題
+response.encoding = 'utf-8'  # 改变编码解决乱码问题
 
 # 检查请求是否成功
 if response.status_code == 200:
     # 使用Beautiful Soup解析页面内容
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # 找到第二个表格元素
-    tables = soup.find_all('table')
-    target_table = tables[1]  # 第二个表格
+    # 找到对应的表格
+    table_index = None
+    keyword = None
 
-    # 初始化关键字位置的变量
-    keyword_position = None
+    if user_input == 'A':
+        table_index = 0
+        keyword = "新台币"
+    elif user_input == 'B':
+        table_index = 1
+        keyword = "100日元/人民币"
+    elif user_input == 'C':
+        table_index = 1
+        keyword = "美元/人民币"
+    elif user_input == 'D':
+        table_index = 0
+        keyword = "日元"
+    elif user_input == 'E':
+        table_index = 0
+        keyword = "日元"
+    elif user_input == 'F':
+        table_index = 0
+        keyword = "美元"
+    elif user_input == 'G':
+        table_index = 0
+        keyword = "美元"
 
-    # 在整个表格中搜索关键字
-    for i, td in enumerate(target_table.find_all('td')):
-        # 如果关键字"100日元/人民币"在<td>元素中
-        if "美元/人民币" in td.get_text():
-            keyword_position = i  # 记录关键字位置
-            break  # 找到后停止寻找
+    if table_index is not None and keyword is not None:
+        # 找到对应的表格元素
+        tables = soup.find_all('table')
+        target_table = tables[table_index]
 
-    if keyword_position is not None:
-        print("关键字位置是表格的第", keyword_position, "个td")
+        # 初始化关键字位置的变量
+        keyword_position = None
+
+        # 在整个表格中搜索关键字
+        for i, td in enumerate(target_table.find_all('td')):
+            # 如果关键字在<td>元素中
+            if keyword in td.get_text():
+                keyword_position = i  # 记录关键字位置
+                break  # 找到后停止寻找
+
+        if keyword_position is not None:
+            print("关键字位置是表格的第", keyword_position, "个td")
+
+            # 获取相应的数值并应用公式
+            td_elements = target_table.find_all('td')
+            if user_input == 'A':
+                result = (float(td_elements[keyword_position + 2].get_text()) + float(td_elements[keyword_position + 4].get_text())) / 200
+            elif user_input == 'B':
+                result = float(td_elements[keyword_position + 1].get_text()) / 100
+            elif user_input == 'C':
+                result = float(td_elements[keyword_position + 1].get_text())
+            elif user_input == 'D':
+                result = float(td_elements[keyword_position + 1].get_text()) / 100
+            elif user_input == 'E':
+                result = float(td_elements[keyword_position + 4].get_text()) / 100
+            elif user_input == 'F':
+                result = float(td_elements[keyword_position + 1].get_text()) / 100
+            elif user_input == 'G':
+                result = float(td_elements[keyword_position + 4].get_text()) / 100
+
+            # 格式化结果为小数点后4位
+            formatted_result = "{:.4f}".format(result)
+            print("目标数值:", formatted_result)
+
+        else:
+            print("未找到关键字")
+
     else:
-        print("未找到关键字")
-
-    #獲取100日元該行的下1格數值
-    td_elements = tables[1].find_all('td')
-    target_td = td_elements[i+1]  # 这里选择了第十个<td>元素
-    target_value = target_td.get_text()
-    numeric_value = float(target_value) / 100
-    print("目标数值:", numeric_value)
+        print("无效的输入")
 
 else:
     print("HTTP请求失败，状态码:", response.status_code)
--- 2.py
+++ 2.py
@@ -3,8 +3,22 @@ import calendar
 import requests
 from bs4 import BeautifulSoup
 
+# 用户输入映射到表格、关键字和公式的字典
+user_input_map = {
+    'A': (0, "新台币", lambda td_elements, keyword_position: (float(td_elements[keyword_position + 2].get_text()) + float(td_elements[keyword_position + 4].get_text())) / 200),
+    'B': (1, "100日元/人民币", lambda td_elements, keyword_position: float(td_elements[keyword_position + 1].get_text()) / 100),
+    'C': (1, "美元/人民币", lambda td_elements, keyword_position: float(td_elements[keyword_position + 1].get_text())),
+    'D': (0, "日元", lambda td_elements, keyword_position: float(td_elements[keyword_position + 1].get_text()) / 100),
+    'E': (0, "日元", lambda td_elements, keyword_position: float(td_elements[keyword_position + 4].get_text()) / 100),
+    'F': (0, "美元", lambda td_elements, keyword_position: float(td_elements[keyword_position + 1].get_text()) / 100),
+    'G': (0, "美元", lambda td_elements, keyword_position: float(td_elements[keyword_position + 4].get_text()) / 100)
+}
+
+# 获取用户输入
+user_input = input("请输入大寫的A~G: ")
+
 # 将字符串"20230901"转换为日期格式
-date_str = "20230801"
+date_str = "20230901"
 new_date = datetime.strptime(date_str, "%Y%m%d")
 
 # 设置为该月的最后一天
@@ -15,38 +29,47 @@ wocha = "https://chl.cn/?" + str(new_date.year) + "-" + str(new_date.month) + "-
 
 # 发送HTTP请求并获取页面内容
 response = requests.get(wocha)
-response.encoding = 'utf-8' #改變編碼解決亂碼問題
+response.encoding = 'utf-8'  # 改变编码解决乱码问题
 
 # 检查请求是否成功
 if response.status_code == 200:
     # 使用Beautiful Soup解析页面内容
     soup = BeautifulSoup(response.text, 'html.parser')
 
-    # 找到第二个表格元素
-    tables = soup.find_all('table')
-    target_table = tables[0]  # 第二个表格
+    # 获取用户输入对应的映射数据
+    table_index, keyword, formula = user_input_map.get(user_input, (None, None, None))
 
-    # 初始化关键字位置的变量
-    keyword_position = None
+    if table_index is not None and keyword is not None and formula is not None:
+        # 找到对应的表格元素
+        tables = soup.find_all('table')
+        target_table = tables[table_index]
 
-    # 在整个表格中搜索关键字
-    for i, td in enumerate(target_table.find_all('td')):
-        # 如果关键字"100日元/人民币"在<td>元素中
-        if "日元" in td.get_text():
-            keyword_position = i  # 记录关键字位置
-            break  # 找到后停止寻找
+        # 初始化关键字位置的变量
+        keyword_position = None
 
-    if keyword_position is not None:
-        print("关键字位置是表格的第", keyword_position, "个td")
+        # 在整个表格中搜索关键字
+        for i, td in enumerate(target_table.find_all('td')):
+            # 如果关键字在<td>元素中
+            if keyword in td.get_text():
+                keyword_position = i  # 记录关键字位置
+                break  # 找到后停止寻找
+
+        if keyword_position is not None:
+            print("关键字位置是表格的第", keyword_position, "个td")
+
+            # 获取相应的数值并应用公式
+            td_elements = target_table.find_all('td')
+            result = formula(td_elements, keyword_position)
+
+            # 格式化结果为小数点后4位
+            formatted_result = "{:.4f}".format(result)
+            print("目标数值:", formatted_result)
+
+        else:
+            print("未找到关键字")
+
     else:
-        print("未找到关键字")
-
-    #獲取100日元該行的下1格數值
-    td_elements = tables[0].find_all('td')
-    numeric_value = float(td_elements[i + 4].get_text()) / 100
-     # 格式化numeric_value为小数点后4位
-    formatted_value = "{:.4f}".format(numeric_value)
-    print("目标数值:", formatted_value)
+        print("无效的输入")
 
 else:
     print("HTTP请求失败，状态码:", response.status_code)
